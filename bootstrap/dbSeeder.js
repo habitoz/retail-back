a015ff818de5e28fd4da9c8ee82fe5dc0c06b3d0
haben mehar <habenmehari71@gmail.com>
2023-08-02 14:16:35 +0300

    db seeder updated

--- bootstrap/dbSeeder.js
+++ bootstrap/dbSeeder.js
@@ -3,7 +3,7 @@ import UserRepo from '../src/interface/user';
 export default async ()=>{
     const data = {
         full_name:"super admin",
-        email: "admin2@admin.com",
+        email: "admin@admin.com",
         role:'manager',
         password: UserRepo.hashPassword('12'),
         status:"Active"
