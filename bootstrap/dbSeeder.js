38cc65d31f3f89e0a6f6ff409067317aec65879a
haben mehar <habenmehari71@gmail.com>
2023-08-24 11:24:05 +0300

    db seeder updated

--- bootstrap/dbSeeder.js
+++ bootstrap/dbSeeder.js
@@ -3,12 +3,13 @@ import UserRepo from '../src/interface/user';
 export default async ()=>{
     const data = {
         full_name:"super admin",
+        username:"admin",
         email: "admin@admin.com",
         role:'manager',
         password: UserRepo.hashPassword('12'),
         status:"Active"
     }
-    const {isPresent} = await UserRepo.checkIfItExists({email:data.email});
+    const {isPresent} = await UserRepo.checkIfItExists({username:data.username});
     if(isPresent) return;
     await UserRepo.insert(data);
 }
